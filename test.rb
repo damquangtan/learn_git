4e2d53f453f05c9e94ec84a7a44e2af754e24178
damquangtan <damquangtan92@gmail.com>
2020-11-28 12:44:30 +0700

    commit tren nhanh master, thay doi file test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
 Tao text o dong nay tren branch master de tao conflic khi merge - dong nay o commit cua nhanh master
 
 Tao text o dong dau tien - dong nay o commit cua nhanh testing
+Dong nay duoc them tai nhanh master
+
