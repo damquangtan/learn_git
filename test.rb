a22e77c675b717c9858d92e6a5421314211ecf1b
damquangtan <damquangtan92@gmail.com>
2020-11-28 14:00:59 +0700

    13:58 commit tren branch server

--- test.rb
+++ test.rb
@@ -2,4 +2,4 @@ Tao text o dong nay tren branch master de tao conflic khi merge - dong nay o com
 
 Tao text o dong dau tien - dong nay o commit cua nhanh testing
 Dong nay duoc them tai nhanh master
-
+13:59 thay doi tren nhanh server
